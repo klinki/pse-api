85d7b0b79e9b2ffe0709ed349e20fcd1c71efbd3
David Klingenberg <dklingenberg@gmail.com>
2019-03-10 19:51:08 +0100

    Add serilog and healthchecks, update DI config

---
+++ PseApi/Configuration/DIConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PseApi.Services;
+
+namespace PseApi.Configuration
+{
+    /// <summary>
+    /// DI Container configuration class.
+    /// </summary>
+    public static class DIConfiguration
+    {
+        /// <summary>
+        /// Extension method registering services to DI container
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<TradeService>();
+            services.AddScoped<StockService>();
+            services.AddScoped<FinSharp.PragueStockExchange.PragueStockExchangeApiClient>();
+
+            return services;
+        }
+    }
+}
