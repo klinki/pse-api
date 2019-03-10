101b91f95f7af5d8baf99fe3733321d26afcd45b
David Klingenberg <dklingenberg@gmail.com>
2019-03-10 22:26:49 +0100

    Add days controller

---
+++ PseApi/Controllers/DaysController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PseApi.Data;
+
+namespace PseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DaysController : ControllerBase
+    {
+        private readonly PseContext _context;
+        private readonly ILogger<DaysController> _logger;
+
+        public DaysController(PseContext context, ILogger<DaysController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        // GET api/values
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<Dataset>>> GetAllDays()
+        {
+            IEnumerable<Dataset> result = await _context.Datasets.ToListAsync();
+
+            return Ok(result.Select(dataset => dataset.Day.ToString("yyyy-MM-dd")));
+        }
+    }
+}
