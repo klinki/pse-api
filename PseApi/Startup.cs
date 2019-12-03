26c80e669dfa21592d758f4c5ce256e2112bab50
David Klingenberg <dklingenberg@gmail.com>
2019-12-03 19:32:04 +0100

    Update startup to use default config for Controllers

--- PseApi/Startup.cs
+++ PseApi/Startup.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using PseApi.Configuration;
 using PseApi.Data;
 using Serilog;
@@ -72,7 +65,7 @@ namespace PseApi
             app.UseHttpsRedirection();
             app.UseEndpoints(endpoints => 
             {
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
 
             app.UseScheduler(lifetime);
