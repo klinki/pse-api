b16f7efbefe56f482726fc9fc3515de3b7224fb0
David Klingenberg <dklingenberg@gmail.com>
2020-01-19 22:16:03 +0100

    Add sentry

--- PseApi/Program.cs
+++ PseApi/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
@@ -18,9 +19,13 @@ namespace PseApi
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseSentry();
+                    webBuilder.UseStartup<Startup>();
+                })
                 .UseSerilog();
     }
 }
