6b752fb415f0c9f4ae0834ba964a37f3bbbf3141
David Klingenberg <dklingenberg@gmail.com>
2020-03-15 18:43:05 +0100

    Add global exception logging

---
+++ PseApi/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace PseApi.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger ?? NullLogger<ErrorController>.Instance;
+        }
+
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            
+            _logger.LogError(context.Error, "Unhandled exception.");
+            
+            return Problem();
+        }
+    }
+}
--- PseApi/Startup.cs
+++ PseApi/Startup.cs
@@ -76,6 +76,8 @@ namespace PseApi
             }
             else
             {
+                app.UseExceptionHandler("/error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
